5cc5423fa63f2c2091274dfce47de5334cd1dded
kendall willard <kendallwillard@hotmail.com>
2019-01-27 21:27:47 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElemenetToBeginningOfArray(myArr[], elem) {
+  var result = [elem, ...myArr]
+  return result
+}
+
+function destructivelyAddElementToBeginningOfArray(myArr[], elem) {
+  
+}
