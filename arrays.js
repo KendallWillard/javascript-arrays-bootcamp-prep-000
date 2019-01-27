11afbb23006445695983aad8d498305ad51cd82c
kendall willard <kendallwillard@hotmail.com>
2019-01-27 21:28:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElemenetToBeginningOfArray(myArr[], elem) {
+function addElemenetToBeginningOfArray(myArr, elem) {
   var result = [elem, ...myArr]
   return result
 }
